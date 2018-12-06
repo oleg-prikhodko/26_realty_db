329e7caba2dbb1587943fff4e19940de08e2a737
Oleg <super.oleg.3000@yandex.ru>
2018-12-06 03:36:12 +0300

    Add pagination

--- server.py
+++ server.py
@@ -1,15 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from persistence import DBManager
 
 app = Flask(__name__)
 
 
+def make_pages_list(current_page, total_pages):
+    if current_page == 1:
+        start, end = current_page, current_page + 3
+    elif current_page == total_pages:
+        start, end = current_page - 2, current_page + 1
+    else:
+        start, end = current_page - 1, current_page + 2
+
+    pages = [
+        page for page in range(start, end) if page >= 1 and page <= total_pages
+    ]
+    return pages
+
+
 @app.route("/")
 def ads_list():
+    try:
+        page = int(request.args.get("page", 1))
+    except ValueError:
+        page = 1
+
     with DBManager() as db_manager:
-        ads = db_manager.get_ads()
-        return render_template("ads_list.html", ads=ads)
+        total_pages = db_manager.get_total_pages()
+        ads = db_manager.get_ads(page=page)
+
+        return render_template(
+            "ads_list.html",
+            ads=ads,
+            current_page=page,
+            pages=make_pages_list(page, total_pages),
+            total_pages=total_pages,
+        )
 
 
 if __name__ == "__main__":
